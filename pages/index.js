0012cae0a9261752f00bae9189ca1f6b51f7532c
Logan <cb58logan@gmail.com>
2022-12-25 01:32:43 +0000

    Successful API integration, but for now only receives most recent retweet

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+import { useRouter } from "next/router";
+import {useEffect, useState} from "react";
+
 export default function Home() {
+    const router = useRouter()
+    const [id,setID] = useState("")
+
+    function goto(e){
+        console.log(id)
+        e.preventDefault();
+        router.push({
+            pathname: "/testAPIPage",
+            query:{
+                id: id
+            }
+        })
+    }
   return (
     <main>
-        <h1>Yo</h1>
+        <h1>Enter a tweet ID</h1>
         <form>
-            <input type="text" id="IDinput"/>
-            <button>Submit</button>
+            <input onChange={(e) => setID(e.target.value)} type="text" id="IDinput"/>
+            <button onClick={goto}>Submit</button>
         </form>
     </main>
   )
--- pages/testAPIPage.js
+++ pages/testAPIPage.js
@@ -1,25 +1,25 @@
-import { TwitterApi } from 'twitter-api-v2';
+import TwitterApi from "twitter-api-v2"
 
 
-const Page = ({ tweets }) => {
-    console.log(tweets)
+const Page = ({ response }) => {
     return (
         <main>
-            <h1>
-                {tweets}
-            </h1>
+            <h1>{response.data[0].id}</h1>
+            <h1>{response.data[0].name}</h1>
+            <h1>{response.data[0].username}</h1>
         </main>
     );
 };
 
-export async function getServerSideProps(context) {
-    const twitterClient = new TwitterApi(process.env.bearerToken);
-    const users = await twitterClient.v2.tweetLikedBy('20');
-    console.log(users.data[0].id);
+export async function getServerSideProps(context) { // runs on page load
+    console.log(context.query.id)
+    const twitterClient = new TwitterApi(process.env.bearer_token); // gets bearer token from env
+    const likedByID = await twitterClient.v2.tweetLikedBy(context.query.id); // gets users who liked tweets by id
+
 
     return {
         props: {
-            users,
+            response: likedByID,
         },
     };
 }
