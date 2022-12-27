5952f810f7d95d7e20fadb0ab2ff45048b8ccdfe
Logan <cb58logan@gmail.com>
2022-12-27 07:58:25 +0000

    Now properly selects a random user from the most recent 1000 likes ( due to API limits)

--- pages/testAPIPage.js
+++ pages/testAPIPage.js
@@ -11,15 +11,58 @@ const Page = ({ response }) => {
 };
 
 export async function getServerSideProps(context) { // runs on page load
-    console.log(context.query.id)
+
     const twitterClient = new TwitterApi(process.env.bearer_token); // gets bearer token from env
-    const likedByID = await twitterClient.v2.tweetLikedBy(context.query.id); // gets users who liked tweets by id
+    const likedByID = await twitterClient.v2.tweetLikedBy(context.query.id,{ asPaginator: true }); // paginated response from twitter api
+
+    let resArr = [iterateRes(likedByID.data)] // 2d arr of all user ID's per page
+
+
+    // twitter API heavily limits calls to 75 per 15 min, will have to work with only doing 10 calls to stay within limit
+    // this means only the most recent 1000 likes are considered
+    // TODO since I am iterating 10 times and then choosing a random page from 0 to 10, unnecessary API calls are being used up. Could run for loop random amount of times and take last?
+    for(let i = 0; i < 9; i++){ // iterating over likedbyID paginations 9 times (+1 from orig call = 10) TODO assess whether this is a good amount
+        if(!likedByID.done && likedByID.rateLimit.remaining !== 0){
+            let pageResult = await likedByID.next(100) // stores new req as own instance, not necessary and confuses things TODO <<<
+            resArr.push(iterateRes(pageResult.data))
+            console.log(pageResult.rateLimit) // TODO debug info remove later
+        }
+    }
+
+    let winner = chooseWinner(resArr)
+    console.log(winner) // TODO debug info remove later
 
     return {
         props: {
-            response: likedByID,
+            response: {
+                data:[{
+                    id:winner.id,
+                    name:winner.name,
+                    username:winner.username
+                }]
+            },
         },
     };
+}
+
+function iterateRes(pageData){ // iterates over page and returns arr of ids
+    let arr = []
+    for(let i = 0; i < pageData.data.length; i++){
+        arr.push(pageData.data[i])
+    }
+    return arr
+}
+
+function chooseWinner(participantArray){ // randomly choose a page, then a user within that page
+    // randomly choosing a page from list of pages.
+    let arrLength = participantArray.length
+    let subarrIndex = Math.floor(Math.random() * arrLength)
+    let winnerArray = participantArray[subarrIndex]
+
+    // choosing a user from chosen page.
+    let winnerArrayLength = winnerArray.length
+    let winnerIndex = Math.floor(Math.random() * winnerArrayLength)
+    return winnerArray[winnerIndex]
 }
 
 export default Page;
