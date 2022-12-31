04893b9c7dba5b8f72028f9335bd792688c10f6d
Logan <cb58logan@gmail.com>
2022-12-31 07:45:31 +0000

    Added ability to pick up to 10 winners, rewrote how winner is chosen. Improved styling.

--- pages/displayWinner.js
+++ pages/displayWinner.js
@@ -1,11 +1,12 @@
 import TwitterApi from "twitter-api-v2"
 import styles from "../styles/displayWinner.module.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { useRouter } from "next/router";
 
 const Page = ({ response }) => {
-    const [winner,setWinner] = useState(response.data[0])
-    const [createdDate,setDate] = useState(new Date(winner.accountAge))
+    const [index,setIndex] = useState(0)
+    const [winner,setWinner] = useState(response.data[0].winnerInfo[0].data)
+    const [createdDate,setDate] = useState(new Date(winner.created_at))
     const [url,setUrl] = useState("https://www.twitter.com/"+winner.username)
     const router = useRouter()
 
@@ -15,8 +16,36 @@ const Page = ({ response }) => {
         })
     }
 
+    useEffect(() => { // iterate through winners when next button is pressed.
+        if(index < response.data[0].winnerInfo.length){
+            let data = response.data[0].winnerInfo[index].data
+            setWinner(data)
+            setDate(new Date(data.created_at))
+            setUrl("https://www.twitter.com/"+data.username)
+        }
+        else{
+            alert(`You have viewed all ${response.data[0].winnerInfo.length} winners!`)
+        }
+    },[index])
 
-    console.log(winner.winnerPage)
+    function HandleIndex(increment){
+        if(increment){
+            if(index < response.data[0].winnerInfo.length-1) {
+                setIndex(prevState => prevState + 1)
+            }
+            else{
+                alert(`You have viewed all ${response.data[0].winnerInfo.length} winners!`)
+            }
+        }
+        else{
+            if(index > 0){
+                setIndex(prevState => prevState-1)
+            }
+            else{
+                alert(`This is the first winner`)
+            }
+        }
+    }
 
     return (
         <main className={styles.winnerMain}>
@@ -26,15 +55,16 @@ const Page = ({ response }) => {
                 <h2>Name: {winner.name}</h2>
                 <h2>Twitter Tag: {winner.username}</h2>
                 <h2>Account created date: {createdDate.toDateString()}</h2>
-                <h2>Private account: {winner.isPrivate ? "Yes" : "No"} </h2>
-                <h2>Verified account: {winner.isVerified ? "Yes" : "No"} </h2>
+                <h2>Private account: {winner.protected ? "Yes" : "No"} </h2>
+                <h2>Verified account: {winner.verified ? "Yes" : "No"} </h2>
 
 
                 <h2>Followers: {winner.public_metrics.followers_count}</h2>
                 <h2>Following: {winner.public_metrics.following_count}</h2>
                 <h2>URL: <a href={url} target="_blank">{url}</a></h2>
 
-                <button className={styles.reRoll}>Re-roll Winner</button>
+                <button onClick={() => HandleIndex(true)} className={styles.nextWinner}>Next Winner</button>
+                <button onClick={() => HandleIndex(false)} className={styles.prevWinner}>Previous Winner</button>
                 <button onClick={returnHome} className={styles.homeButton}>Return Home</button>
 
             </div>
@@ -42,17 +72,36 @@ const Page = ({ response }) => {
     );
 };
 
-// I know its probably not good to run this much code in the getServerSideProps but when I try streamlining the process by using api routes etc it fails
-// I think its something to do with the library only working on server side
+// I know It's probably not good to run this much code in the getServerSideProps but when I try streamlining the process by using api routes etc it fails
+// I think It's something to do with the library only working on server side
 export async function getServerSideProps(context) {
 
-    const twitterClient = new TwitterApi(process.env.bearer_token); // gets bearer token from env
-    let likedByID = null
-
-    // error handling for API limit. Any other error returns 404.
-    try {
-        likedByID = await twitterClient.v2.tweetLikedBy(context.query.id,{ asPaginator: true }); // paginated response from twitter api TODO can be adapted later to get likes, retweets or replies depending on chosen
+    if(outOfRangeValues(context)){ // catching invalid values inserted via URL.
+        return {
+            redirect: {
+                destination: '/errorPages/tweet404',
+                permanent: false,
+            },
+        };
     }
+
+
+
+    const twitterClient = new TwitterApi(process.env.bearer_token); // gets bearer token from env
+    let apiCall = null
+
+    try {
+        switch(context.query.requirement){
+            case "liked":
+                apiCall = await twitterClient.v2.tweetLikedBy(context.query.id,{ asPaginator: true }); // paginated response from twitter api
+                break
+            case "retweeted":
+                apiCall = await twitterClient.v2.tweetRetweetedBy(context.query.id,{ asPaginator: true }); // paginated response from twitter api
+                break
+            default:
+                apiCall = await twitterClient.v2.tweetLikedBy(context.query.id,{ asPaginator: true });
+        }
+    } // error handling for API limit. Any other error returns 404.
     catch (ApiResponseError){
         switch (ApiResponseError.code){
             case 429: // Error for reaching API limit
@@ -69,7 +118,7 @@ export async function getServerSideProps(context) {
         }
     }
     // API does not return 404 if the tweet does not exist, so must handle it here.
-    if(likedByID.errors.length > 0){
+    if(apiCall.errors.length > 0 || apiCall.data.meta.result_count === 0){
         return {
             redirect: {
                 destination: '/errorPages/tweet404',
@@ -77,67 +126,60 @@ export async function getServerSideProps(context) {
             },
         };
     }
-
-    let resArr = [iterateRes(likedByID.data)] // 2d arr of all user ID's per page
-
     // twitter API heavily limits calls to 75 per 15 min, will have to work with only doing a maximum of 20 calls to stay within limit this means at most only the most recent 2000 likes are considered
     let consideredPages = Math.floor(Math.random() * 20)
-    for(let i = 0; i < consideredPages; i++){ // iterating over likedbyID paginations up to 20 times.
-        if(!likedByID.done && likedByID.rateLimit.remaining > 10){
-            let pageResult = await likedByID.next(100)
-            resArr.push(iterateRes(pageResult.data))
-            console.log(pageResult.rateLimit) // TODO debug info remove later
+    for(let i = 0; i < consideredPages; i++){ // iterating over pagination up to 20 times.
+        if(!apiCall.done && apiCall.rateLimit.remaining > 10){
+            await apiCall.fetchNext(100)
         }
         else{
             break
         }
     }
 
-    let winner = chooseWinner(resArr)
+    let winnerIDArr = []
+    let participants = apiCall.data.data
+    for(let i = 0; i < Number.parseInt(context.query.numOfWinners); i++){
+        winnerIDArr.push(chooseWinner(participants).id)
+    }
 
-    console.log(winner.username) // TODO debug info remove later
+    let winnerInfoArr = []
+    for(let i = 0; i < winnerIDArr.length;i++){
+        let winnerDetails = await twitterClient.v2.user(winnerIDArr[i], { 'user.fields':
+                ['created_at','description','entities','id','location','name','pinned_tweet_id',
+                    'profile_image_url', 'protected','public_metrics','url','username','verified','withheld'] });
+        winnerInfoArr.push(winnerDetails)
+    }
 
-    const winnerDetails = await twitterClient.v2.user(winner.id, { 'user.fields':
-            ['created_at','description','entities','id','location','name','pinned_tweet_id',
-                'profile_image_url', 'protected','public_metrics','url','username','verified','withheld'] });
 
     return {
         props: {
             response: {
                 data:[{
-                    id:winner.id,
-                    name:winner.name,
-                    username:winner.username,
-                    public_metrics:winnerDetails.data.public_metrics,
-                    isVerified:winnerDetails.data.verified,
-                    isPrivate:winnerDetails.data.protected,
-                    accountAge: winnerDetails.data.created_at,
+                    winnerInfo:winnerInfoArr
                 }]
             },
         },
     };
 }
 
-// iterates over a given page and returns arr of ids, name and tag of every user in that page
-function iterateRes(pageData){
-    let arr = []
-    for(let i = 0; i < pageData.data.length; i++){
-        arr.push(pageData.data[i])
-    }
-    return arr
-}
 
-// randomly chooses a page, then a user within that page and returns the winner user as well as the page of the winner
-function chooseWinner(participantArray){
-    // randomly choosing a page from list of pages.
-    let arrLength = participantArray.length
-    let pageIndex = Math.floor(Math.random() * arrLength)
-    let winnerPage = participantArray[pageIndex]
-
-    let pageLength = winnerPage.length
+// Given a page, returns a user within that page at random
+function chooseWinner(participants){
+    let pageLength = participants.length
     let winnerIndex = Math.floor(Math.random() * pageLength)
-    winnerPage.splice(winnerIndex,1)
-    return winnerPage[winnerIndex]
+    return participants[winnerIndex]
+}
+// Error checking for params that exceed limit, since this can be manually modified in the URL
+function outOfRangeValues(context){
+    let winnerNum = 0
+    try{
+        winnerNum = Number.parseInt(context.query.numOfWinners)
+    }
+    catch (e){
+        return false
+    }
+    return winnerNum > 10 || winnerNum < 1 || !Number.isInteger(winnerNum);
 }
 
 export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,31 @@
 import { useRouter } from "next/router";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import styles from "../styles/index.module.css"
 
 export default function Index() {
     const router = useRouter()
     const [URL,setURL] = useState("")
+    const [winnerNum,setWinnerNum] = useState(0)
     const [loading,setLoading] = useState(false)
 
-    function goto(parsedID){
+    function goto(parsedID,requirement){
         setLoading(true)
         router.push({
             pathname: "/displayWinner",
             query:{
-                id: parsedID
+                id: parsedID,
+                requirement:requirement,
+                numOfWinners:winnerNum
             }
         })
     }
 
-    function parseURL(){
+    function parseURL(requirement){
         let parsedURL = URL.split("/status/")[1]
-        goto(parsedURL)
+        goto(parsedURL,requirement)
     }
 
-    function verification(e){
+    function verification(e,requirement){
         e.preventDefault();
         if(URL.length=== 0){
             alert("Please Enter a URL")
@@ -30,8 +33,14 @@ export default function Index() {
         else if(!URL.match(`(/twitter.com/)`)){
             alert("This is not a valid URL")
         }
+        else if(winnerNum === 0){
+            alert("Please enter a number of winners.")
+        }
+        else if(winnerNum > 10 || winnerNum < 0 || !Number.isInteger(winnerNum)){
+            alert("Not a valid number of winners.")
+        }
         else{
-            parseURL()
+            parseURL(requirement)
         }
     }
 
@@ -39,14 +48,16 @@ export default function Index() {
       !loading ?
           <main className={styles.indexBody}>
               <form className={styles.inputForm}>
-                  <h3 className={styles.header}>Enter a tweet URL to get started!</h3>
+                  <h3 className={styles.header}>Enter a tweet URL to get started</h3>
                   <label className={styles.text}>Tweet URL: </label>
                   <input type="text" className={styles.URLInput} onChange={(e) => setURL(e.target.value)}/>
 
+                  <h3>Enter the number of winners (maximum 10):</h3>
+                  <input type="text" className={styles.winnerInput} onChange={(e) => setWinnerNum(Number.parseInt(e.target.value))}/>
+
                   <h3>select a winner from:</h3>
-                  <button className={styles.likedButton} onClick={verification}>Liked users</button>
-                  <button className={styles.rtButton} onClick={verification}>Retweeting users</button>
-                  <button className={styles.repliedButton} onClick={verification}>Replied users</button>
+                  <button className={styles.likedButton} onClick={(e) => verification(e,"liked")}>Liked users</button>
+                  <button className={styles.rtButton} onClick={(e) => verification(e,"retweeted")}>Retweeting users</button>
               </form>
           </main>
           :
